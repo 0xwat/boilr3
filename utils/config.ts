1e40ea2edd38b16d357e44508e81087d6523da96
0xwat <134099711+0xwat@users.noreply.github.com>
2023-05-20 06:44:16 -0400

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -3,9 +3,9 @@ import { mainnet, polygon, sepolia, polygonMumbai } from 'wagmi/chains';
 export const ETH_CHAINS = [mainnet, polygon, sepolia, polygonMumbai];
 export const WALLET_CONNECT_PROJECT_ID = 'YOUR_WALLET_CONNECT_PROJECT_ID';
 
-export const SITE_NAME = 'boilr3';
+export const SITE_NAME = '0xwat';
 export const SITE_DESCRIPTION =
-	'The Ultimate Next.js dApp Boilerplate with RainbowKit, Tailwind CSS & WAGMI';
+	'Next.js dApp (boilr3)';
 export const SITE_URL = 'https://boilr3.vercel.app';
 
 export const SOCIAL_TWITTER = 'Envoy_1084';
